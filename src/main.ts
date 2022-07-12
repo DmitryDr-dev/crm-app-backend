61ad80e53dac9e658586c8cfa2137f235e3eaf32
DmitryDr-dev <79312440+DmitryDr-dev@users.noreply.github.com>
2022-07-12 19:02:43 +0300

    feat: add contacts to main ioc container

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { Container } from 'inversify';
 import { appBindings, APP_TYPES } from './common/ioc/app-bindings';
+import { contactsBindings } from './common/ioc/contact-bindings';
 import { App } from './app';
 
 async function bootstrap(): Promise<void> {
   const appContainer = new Container();
-  appContainer.load(appBindings);
+  appContainer.load(appBindings, contactsBindings);
   const app = appContainer.get<App>(APP_TYPES.App);
   await app.runServer();
 }
