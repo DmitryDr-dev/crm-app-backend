a4594970c2d07bf74ec89ea76f6a9492a008eda6
DmitryDr-dev <79312440+DmitryDr-dev@users.noreply.github.com>
2022-07-12 15:54:32 +0300

    feat: add error handler service to ioc container

--- src/common/ioc/app-bindings/app-bindings.config.ts
+++ src/common/ioc/app-bindings/app-bindings.config.ts
@@ -2,10 +2,17 @@ import { ContainerModule, interfaces } from 'inversify';
 import { APP_TYPES } from './app-bindings.types';
 import { App } from '../../../app';
 import { LoggerService, ILoggerService } from '../../../app-modules/logger';
+import {
+  ErrorHandlerService,
+  IErrorHandlerService,
+} from '../../../app-modules/error/error-handler';
 
 export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
   bind<App>(APP_TYPES.App).to(App).inSingletonScope;
   bind<ILoggerService>(APP_TYPES.ILoggerService)
     .to(LoggerService)
+    .inSingletonScope();
+  bind<IErrorHandlerService>(APP_TYPES.IErrorhandlerService)
+    .to(ErrorHandlerService)
     .inSingletonScope();
 });
