43352b5aada5731d29bf61bf6ebfebd7cc1da5e6
DmitryDr-dev <79312440+DmitryDr-dev@users.noreply.github.com>
2022-07-13 15:22:39 +0300

    feat: add user & auth bindings to main container

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { Container } from 'inversify';
 import { appBindings, APP_TYPES } from './common/ioc/app-bindings';
 import { contactsBindings } from './common/ioc/contact-bindings';
+import { authBindings } from './common/ioc/auth-bindings';
+import { usersBindings } from './common/ioc/users-bindings';
 import { App } from './app';
 
 async function bootstrap(): Promise<void> {
   const appContainer = new Container();
-  appContainer.load(appBindings, contactsBindings);
+  appContainer.load(appBindings, contactsBindings, authBindings, usersBindings);
   const app = appContainer.get<App>(APP_TYPES.App);
   await app.runServer();
 }
