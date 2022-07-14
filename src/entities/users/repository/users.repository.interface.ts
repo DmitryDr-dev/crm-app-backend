9ae16195f077ba7f04a507e300522b53cd570b43
DmitryDr-dev <79312440+DmitryDr-dev@users.noreply.github.com>
2022-07-14 20:33:57 +0300

    feat: add users repository

--- src/entities/users/repository/users.repository.interface.ts
+++ src/entities/users/repository/users.repository.interface.ts
@@ -1,1 +1,15 @@
-export interface IUsersRepository {}
+import { UserDocumentType } from '../model/index';
+import { SignUpUserRequestDto } from '../../../auth/dto/signUpUser';
+
+export interface IUsersRepository {
+  createUser: (body: SignUpUserRequestDto) => Promise<UserDocumentType | null>;
+
+  findByUserByEmail: (email: string) => Promise<UserDocumentType | null>;
+
+  updateToken: (
+    id: string,
+    token: string | null,
+  ) => Promise<UserDocumentType | null>;
+
+  findUserById: (userId: string) => Promise<UserDocumentType | null>;
+}
